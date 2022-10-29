1e3d4ddac93161c3aae897dc0ca673e6a2d5ed4f
Sani J <binabdjalal@gmail.com>
2022-10-29 09:10:39 +0800

    Added code to attack enemy.

--- js/script.js
+++ js/script.js
@@ -2,43 +2,62 @@ function buttonStartClicked() {
     window.location.href = "html/game.html";
 }
 
-var enemyHitpoint = 20
+class Enemy {
+    constructor(name, hitpoint) {
+      this.name = name
+      this.hitpoint = hitpoint
+    }
+}
+
+class Player {
+    constructor(hitpoint) {
+        this.hitpoint = hitpoint
+      }
+}
+
 var playerHitpoint = 20
 var turnCount = 0
 var battleLog = []
 var isPlayerTurn = true
+var enemies = []
+var heroes = []
+var attackIndex = 0
+var player
 
 function attackButtonClicked() {
     turnCount++
     if (isPlayerTurn) {
-        document.getElementById("actionButton").innerHTML = "Serang"
+        document.getElementById("actionButton").innerHTML = "Sedia"
         let damageByPlayer = calculateDamageDealtByPlayer()
-        enemyHitpoint = enemyHitpoint - damageByPlayer
         var log = "Orang baik serang!"
         if (damageByPlayer > 0) {
             log = log + " Pukul " + damageByPlayer + " nyawa!"
         } else {
             log = log + " Eh tak kena!"
         }
+
+        var enemy = enemies[attackIndex]
+        enemy.hitpoint = enemy.hitpoint - damageByPlayer
+
+        console.log(log)
         battleLog.push(log)
     } else {
-        document.getElementById("actionButton").innerHTML = "Sedia"
+        document.getElementById("actionButton").innerHTML = "Serang"
         var log = "Orang jahat serang!"
         battleLog.push(log)
+        console.log(log)
     }
 
     isPlayerTurn = !isPlayerTurn
 
-    document.getElementById("jahat.hp").textContent = enemyHitpoint
-    document.getElementById("baik.hp").textContent = playerHitpoint
-
-    
     var logOutput = battleLog.reduce(reduceLogsToOutput)
     if (battleLog.length == 1) {
         logOutput = "<li>" + logOutput + "</li>"
     }
     logOutput = "<ul>" + logOutput + "</ul>"
     document.getElementById("battlelog").innerHTML = logOutput
+
+    updateState()
 }
 
 function reduceLogsToOutput(total, value, index, array) {
@@ -50,9 +69,53 @@ function reduceLogsToOutput(total, value, index, array) {
 
 function calculateDamageDealtByPlayer() {
     let hitRoll = Math.floor(Math.random() * 10)
+    console.log("Hit Roll: " + hitRoll)
     if (hitRoll < 5) {
         return 0
     }
     let damageRoll = Math.floor(Math.random() * 4)
+    console.log("Damage Roll: " + damageRoll)
     return damageRoll
+}
+
+function createEnemyHtml(enemy, order) {
+    const htmlContent = "<div id=\"enemy." + order + "\">" + enemy.name + " " + enemy.hitpoint + " nyawa <button>Pilih</button>" + "</div>"
+    return htmlContent
+}
+
+function createBaikHtml(watak) {
+    const htmlContent = "<div id=\"baik\">Nyawa orang baik: " + watak.hitpoint + " nyawa" + "</div>"
+    return htmlContent
+}
+
+function updateState() {
+    var enemyHtml = ""
+    enemies.forEach( (value, index, array) => {
+        enemyHtml += createEnemyHtml(value, index)
+    })
+    document.getElementById("senarai.jahat").innerHTML = enemyHtml
+    
+    const htmlBaik = createBaikHtml(player)
+    document.getElementById("senarai.baik").innerHTML = htmlBaik
+}
+
+function pageLoaded() {
+    enemies.push(new Enemy("Jahat 1", 20))
+    enemies.push(new Enemy("Abu", 22))
+    enemies.push(new Enemy("Gorgon", 14))
+
+    var enemyHtml = ""
+    enemies.forEach( (value, index, array) => {
+        enemyHtml += createEnemyHtml(value, index)
+    })
+    document.getElementById("senarai.jahat").innerHTML = enemyHtml
+
+    player = new Player(20)
+    const htmlBaik = createBaikHtml(player)
+
+    document.getElementById("senarai.baik").innerHTML = htmlBaik
+
+    document.addEventListener('keydown', function(event) {
+        console.log("Key pressed " + event.key)
+    })
 }
